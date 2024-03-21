eb6115e62defa0e3f481fa11d18440f0f4a45a5e
Sonika Bansal <sonika.bansal@hcl.com>
2024-03-21 10:53:44 +0530

    chat bot code pushed

---
+++ src/App.js
@@ -0,0 +1,204 @@
+import { useEffect, useState, useRef } from 'react';
+import { stripHtml } from 'string-strip-html';
+import axios from 'axios';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import minimizeImg from './images/minimize.svg';
+import refreshImg from './images/refresh.svg';
+import micImg from './images/mic.svg';
+import sendArrowImg from './images/sendArrow.svg';
+import chatBotImg from './images/chatBot.svg';
+import smileyImg from './images/smiley.svg';
+import attachmentImg from './images/attachment.svg';
+import './App.css';
+
+function App() {
+  const [chatHistory, setChatHistory] = useState([{ messageType: 'R', message: 'Hello! How can I assist you today?' }])
+  const [message, setMessage] = useState('');
+  let sessionId = useRef('');
+  // let inactivityTime = function () {
+  //   console.log("inside inactivity");
+  //   let time;
+  //   window.onload = resetTimer;
+  //   document.onmousemove = resetTimer;
+  //   document.onkeypress = resetTimer;
+  //   function logout() {
+  //     console.log("You are now logged out.")
+  //     chatHistory.push({ messageType: 'R', message: "Your session is expired" });
+  //     setChatHistory([...chatHistory]);
+  //     sessionId.current = 'logout';
+  //   }
+  //   function resetTimer() {
+  //     clearTimeout(time);
+  //     time = setTimeout(logout, 60000)
+  //   }
+  // };
+  // window.onload = function() {
+  //   inactivityTime();
+  // }
+  // // console.log('Please wait...');
+
+  useEffect(() => {
+    // sessionId.current = Math.random().toString(36).substring(2, 14);
+    // // sessionId.current = Math.floor(100000000 + Math.random() * 900000000)
+    // console.log("useEffect", sessionId.current);
+    // callMessagingAPI("get_session_id");
+    axios.post("https://4ztrb2qdny7zrjeqwkf5vnvwbm0vapyp.lambda-url.ap-south-1.on.aws/", {
+      user_input: 'get_session_id',
+      sessionId: sessionId.current
+    }, {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }).then(response => {
+      // console.log("sessionid", response.data);
+      sessionId.current = response.data;
+      // chatHistory.push({ messageType: 'R', message: response.data });
+      // setChatHistory([...chatHistory]);
+    }).catch(err => {
+      // chatHistory.push({ messageType: 'R', message: "Sorry, we couldn't help you this time due to server issue" });
+      // setChatHistory([...chatHistory]);
+    });
+  }, [sessionId])
+
+  useEffect(() => {
+
+    var el = document.getElementById('centerPanel');
+    el.scrollTop = el.scrollHeight - el.clientHeight;
+    var confirmButton = document.getElementById("btnConfirm");
+
+    if (confirmButton) {
+      confirmButton.addEventListener("click", handleConfirm);
+      confirmButton.removeAttribute("id");
+      return () => {
+        confirmButton.removeEventListener('click', handleConfirm);
+      }
+    }
+  }, [chatHistory]);
+
+  const commands = [
+    {
+      command: 'confirm',
+      callback: () => {
+        setMessage('');
+        handleConfirm();
+      }
+    },
+  ]
+
+  const {
+    transcript,
+    listening,
+    finalTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  // } = useSpeechRecognition({ commands });
+
+  useEffect(() => {
+    setMessage(transcript);
+  }, [transcript])
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  function handleChange(event) {
+    setMessage(event.target.value);
+  };
+  // console.log("sess2", sessionId.current);
+  function callMessagingAPI(input) {
+    // axios.post("https://ltiml0gjc5.execute-api.ap-south-1.amazonaws.com/dev/conversation",{
+    // axios.post("https://bquodjtujh.execute-api.ap-south-1.amazonaws.com/dev/conversation",{
+    axios.post("https://4ztrb2qdny7zrjeqwkf5vnvwbm0vapyp.lambda-url.ap-south-1.on.aws/", {
+      user_input: input,
+      sessionId: sessionId.current
+    }, {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }).then(response => {
+      chatHistory.push({ messageType: 'R', message: response.data });
+      setChatHistory([...chatHistory]);
+      handleSpeech(response.data);
+    }).catch(err => {
+      console.log("error", err);
+      chatHistory.push({ messageType: 'R', message: "Sorry, we couldn't help you this time due to server issue" });
+      setChatHistory([...chatHistory]);
+    });
+  }
+
+  function handleSendMessage() {
+    if (message) {
+      chatHistory.push({ messageType: 'S', message: message });
+      setChatHistory([...chatHistory])
+      setMessage('');
+      callMessagingAPI(message);
+    }
+  }
+
+  function handleConfirm(event) {
+    // event.stopPropagation();
+    var confirmButton = document.getElementsByClassName("btnBlue");
+    if (confirmButton[0]) {
+      confirmButton[0].classList.add("btnGrey");
+      confirmButton[0].disabled = true;
+      confirmButton[0].classList.remove("btnBlue");
+    }
+    callMessagingAPI('confirm_button');
+  }
+
+  function handleEnterKey(event) {
+    if (event.keyCode === 13) {
+      handleSendMessage();
+    }
+  }
+
+  function handleSpeech(msg = "") {
+    window.speechSynthesis.cancel();
+    const utterance = new SpeechSynthesisUtterance(stripHtml(msg).result);
+    const voices = speechSynthesis.getVoices();
+    if (!voices.length) {
+      utterance.voice = voices[0];
+    }
+    // utterance.lang = 'fr';
+    // utterance.rate = 2;
+    speechSynthesis.speak(utterance);
+  }
+
+  return (
+    <div className="container">
+      <div className="bar">
+        <img src={chatBotImg} className='chatBotLogo' />
+        <span className="barHeading">GenAI<span className="botText">Bot</span></span>
+        <span className="barRight">
+          <img src={refreshImg} />
+          <img src={minimizeImg} className='minimizeImg' />
+        </span>
+      </div>
+      <div className='centerPanel' id="centerPanel" >
+        {
+          chatHistory.map((msgRow, index) => {
+            return <>
+              {msgRow.messageType === "R" &&
+                  <div id="receivedMsg" className="receivedMsgSection" dangerouslySetInnerHTML={{ __html: msgRow.message }} />
+              }
+              {msgRow.messageType === "S" && <div className="sentMsgSection" >{msgRow.message}</div>}
+            </>
+          })
+        }
+      </div>
+      <div className="inputSection">
+        <span className='msgSectionLeft'>
+          <img src={smileyImg} className='smileyImg' />
+          <input type="text" placeholder='Type a message' value={message} className='inputBox' onChange={handleChange} onKeyUp={handleEnterKey} />
+          <img src={attachmentImg} className='attachmentImg' />
+        </span>
+        <span className='msgSectionRight'>
+          <span className='mic'><img src={micImg} className="micImage" onMouseDown={SpeechRecognition.startListening} onMouseUp={SpeechRecognition.stopListening} /></span>
+          <span className="sendArrow"><img src={sendArrowImg} className='sendArrowImg' onClick={handleSendMessage} /></span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
